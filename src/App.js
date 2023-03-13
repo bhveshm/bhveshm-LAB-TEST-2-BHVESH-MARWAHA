677a304711da6d1236d3b22f2c94a91278e3f419
QuizNoz <marwaha.bhvesh@gmail.com>
2023-03-13 16:41:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ProductTable from "./ProductTable";
+import AddProductForm from "./AddProductForm";
+import "./index.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const handleAddProduct = newProduct => {
+    setProducts([...products, newProduct]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-medium mb-4">Product Catalog</h1>
+      <AddProductForm onAddProduct={handleAddProduct} />
+      <ProductTable products={products} />
     </div>
   );
 }
